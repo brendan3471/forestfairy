f4714ee02437c113f2438995aa72ba8784f965d3
brendan3471 <brendanmclelland@gmail.com>
2026-04-17 11:11:23 +1200

    fix duplicate  request

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Order;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
