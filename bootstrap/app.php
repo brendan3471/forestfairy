820074fe284f5fce91da957b9cf377caa2db468c
brendan3471 <brendanmclelland@gmail.com>
2026-04-16 13:44:59 +1200

    fix connect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'stripe/webhook',
-            'connect/webhook',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
